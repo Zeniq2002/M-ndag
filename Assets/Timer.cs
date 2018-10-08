5db2fc7a1b0b6fef2001d7b26202fd37accc96d0
Dejan Bavdaz <dejan.bavdaz@elev.ga.lbs.se>
2018-10-08 10:50:51 +0200

    Måndag

---
+++ Assets/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    public float timerText;
+    public float timer;
+    public float secunder = 1;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log(secunder);
+
+        timerText = timerText + Time.deltaTime;
+        if (timerText >= secunder)
+        {
+            print(timerText);
+            secunder = secunder + 1;
+        }
+	}
+}
